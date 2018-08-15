68f38795774925b51bf63147dffe626e5e23f1e5
Nick Pisciotta <npisciot@thoughtworks.com>
2018-08-14 22:59:35 -0400

    Completed Anuglar Heroes Tutorial

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero'; 
+import { HeroService } from '../hero.service'; 
+import { Location } from '@angular/common'; 
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService, 
+    private location: Location
+  ) { }
+
+  ngOnInit(): void {
+    this.getHero(); 
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero); 
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack()); 
+  }
+
+}
